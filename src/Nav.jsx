0e35ea21e4d269d52565916dd583240e6718f2eb
milankj <milanjain005@gmail.com>
2022-08-12 18:28:38 +0530

    logo click home page

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -9,12 +9,12 @@ export default function Nav(){
   }
     return(
         <div className='navbar-custom d-flex justify-content-between p-3'>
-            <div className='d-flex brand' onClick={()=>{nagivate('/home')}}>
+            <div className='d-flex brand' onClick={()=>{navigate('/home')}}>
             <img className='world-logo'  src='https://cdn-icons-png.flaticon.com/128/921/921439.png'/>
             <h3 className='journal-name mt-1'>my travel Journal.</h3>
             </div>
             <div className='align-self-end ml-auto -px-5'>
-            <button className='btn btn-light ' onClick={logout}><i class="bi bi-box-arrow-left"></i></button>
+            <button className='btn btn-light ' onClick={logout}><i className="bi bi-box-arrow-left"></i></button>
             </div>
         </div>
     )
